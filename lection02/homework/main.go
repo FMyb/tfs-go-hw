670a0d3b04d8031c2ce5f8b6629e933e7ddb2181
Yaroslav Ilin <neznayu.ucoz.org@gmail.com>
2021-09-26 15:37:01 +0300

    add solve for hw2

---
+++ lection02/homework/main.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"time"
+)
+
+//
+//type OperationBody struct {
+//	Type string `json:"type"`
+//	Value int `json:"value"`
+//	Id interface{} `json:"id"`
+//}
+
+type Operation struct {
+	Company   string      `json:"company"`
+	Id        interface{} `json:"id"`
+	Type      string      `json:"type"`
+	Value     *int        `json:"value"`
+	CreatedAt *time.Time  `json:"created_at"`
+}
+
+type CompanyOperation struct {
+	Company           string        `json:"company"`
+	OperationsCount   int           `json:"valid_operations_count"`
+	Balance           int           `json:"balance"`
+	InvalidOperations []interface{} `json:"invalid_operations,omitempty"`
+}
+
+func (op *Operation) UnmarshalJSON(data []byte) error {
+	var f interface{}
+	err := json.Unmarshal(data, &f)
+	if err != nil {
+		return err
+	}
+	m := f.(map[string]interface{})
+	operationMap, ok := m["operation"]
+	if ok {
+		v := operationMap.(map[string]interface{})
+		id, ok := v["id"]
+		if ok {
+			switch id.(type) {
+			case string:
+				op.Id = id.(string)
+			case int:
+				op.Id = id.(int)
+			}
+			op.Id = id
+		}
+		value, ok := v["value"]
+		if ok {
+			switch value.(type) {
+			case string:
+				parseVal, err := strconv.Atoi(value.(string))
+				if err == nil {
+					op.Value = &parseVal
+				}
+			case float64:
+				parseVal := int(value.(float64))
+				op.Value = &parseVal
+			case int:
+				parseVal := value.(int)
+				op.Value = &parseVal
+			}
+		}
+		creationAt, ok := v["created_at"]
+		if ok {
+			create, _ := time.Parse(time.RFC3339, creationAt.(string))
+			op.CreatedAt = &create
+		}
+		t, ok := v["type"]
+		if ok {
+			parseT, ok := t.(string)
+			if ok {
+				switch parseT {
+				case "income", "outcome", "+", "-":
+					op.Type = parseT
+				}
+			}
+		}
+	}
+	id, ok := m["id"]
+	if ok {
+		switch id.(type) {
+		case string:
+			op.Id = id.(string)
+		case int:
+			op.Id = id.(int)
+		}
+		op.Id = id
+	}
+	value, ok := m["value"]
+	if ok {
+		switch value.(type) {
+		case string:
+			parseVal, err := strconv.Atoi(value.(string))
+			if err == nil {
+				op.Value = &parseVal
+			}
+		case float64:
+			parseVal := int(value.(float64))
+			op.Value = &parseVal
+		case int:
+			parseVal := value.(int)
+			op.Value = &parseVal
+		}
+	}
+	t, ok := m["type"]
+	if ok {
+		parseT, ok := t.(string)
+		if ok {
+			switch parseT {
+			case "income", "outcome", "+", "-":
+				op.Type = parseT
+			}
+		}
+	}
+	company, ok := m["company"]
+	if ok {
+		op.Company = company.(string)
+	}
+	creationAt, ok := m["created_at"]
+	if ok {
+		create, _ := time.Parse(time.RFC3339, creationAt.(string))
+		op.CreatedAt = &create
+	}
+	return nil
+}
+
+func main() {
+	var f2 *os.File
+	defer f2.Close()
+	var err error
+	inputFile := flag.String("file", "", "path to json source")
+	flag.Parse()
+	if inputFile != nil && *inputFile != "" {
+		f2, err = os.Open(*inputFile)
+		if err != nil {
+			fmt.Println("Can't open file as flag file")
+			return
+		}
+	} else {
+		inputFile := os.Getenv("FILE")
+		if inputFile != "" {
+			f2, err = os.Open(inputFile)
+			if err != nil {
+				fmt.Println("Can't open file as ENV FILE")
+			}
+		} else {
+			f2 = os.Stdin
+		}
+	}
+	data, err := ioutil.ReadAll(f2)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var operations []Operation
+	err = json.Unmarshal(data, &operations)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	operationsMap := make(map[string][]Operation)
+	for _, operation := range operations {
+		if operation.Company == "" {
+			continue
+		}
+		operationList, ok := operationsMap[operation.Company]
+		if !ok {
+			operationList = make([]Operation, 0, 5)
+		}
+		operationsMap[operation.Company] = append(operationList, operation)
+	}
+	result := make([]CompanyOperation, 0, len(operationsMap))
+	for company, operationList := range operationsMap {
+		comp := CompanyOperation{Company: company}
+		for _, operation := range operationList {
+			if operation.CreatedAt == nil || operation.Id == nil {
+				continue
+			}
+			if operation.Type == "" || operation.Value == nil {
+				comp.InvalidOperations = append(comp.InvalidOperations, operation.Id)
+			} else {
+				comp.OperationsCount++
+				switch operation.Type {
+				case "+", "income":
+					comp.Balance += *operation.Value
+				case "-", "outcome":
+					comp.Balance -= *operation.Value
+				}
+			}
+		}
+		result = append(result, comp)
+	}
+	str, err := json.MarshalIndent(result, "", "	")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	f, _ := os.Create("lection02/homework/out.json")
+	defer f.Close()
+	_, err = fmt.Fprint(f, string(str))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
